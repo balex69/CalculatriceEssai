9e3f6007f197f5ec0df1e99799ded1197750e1c4
balex69 <119514045+balex69@users.noreply.github.com>
2022-12-14 16:36:09 +0100

    instanciation et fenetre

--- src/Main.java
+++ src/Main.java
@@ -21,13 +21,61 @@ public class Main implements ActionListener{
 
         fenetre = new JFrame("Calculatrice basique");
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenetre.setSize(380, 500);
+        fenetre.setSize(360, 500);
         fenetre.setLayout(null);
 
         zoneAffichage = new JTextField();
-        zoneAffichage.setBounds(40, 20, 280, 40);
+        zoneAffichage.setBounds(40, 20, 260, 40);
         zoneAffichage.setFont(policeUtilisee);
         zoneAffichage.setEditable(false);
+
+        boutonAddi = new JButton("+"); // instanciation des boutons de fonction
+        boutonSous = new JButton("-");
+        boutonMult = new JButton("x");
+        boutonDivi = new JButton("%");
+        boutonDeci = new JButton(".");
+        boutonEgal = new JButton("=");
+        boutonSupp = new JButton("<-");
+        boutonEffa = new JButton("C");
+
+        boutonsFonctions[0] = boutonAddi; // ajout à l'array
+        boutonsFonctions[1] = boutonSous;
+        boutonsFonctions[2] = boutonMult;
+        boutonsFonctions[3] = boutonDivi;
+        boutonsFonctions[4] = boutonDeci;
+        boutonsFonctions[5] = boutonEgal;
+        boutonsFonctions[6] = boutonSupp;
+        boutonsFonctions[7] = boutonEffa;
+
+        for(int i = 0; i < 8; i++) { // ajout de l'actionlistener à chaque bouton
+            boutonsFonctions[i].addActionListener(this);
+            boutonsFonctions[i].setFont(policeUtilisee);
+            boutonsFonctions[i].setFocusable(false);
+        }
+
+        /*boutonsChiffres[0] = new JButton("0"); // utilisé pour créer les boutons chiffres, mais comme anonymes, peut juste instancier avec un for
+        boutonsChiffres[1] = new JButton("1");  // fonctionne très bien car chiffres, donc peut simplement avoir une itération et ++
+        boutonsChiffres[2] = new JButton("2");
+        boutonsChiffres[3] = new JButton("3");
+        boutonsChiffres[4] = new JButton("4");
+        boutonsChiffres[5] = new JButton("5");
+        boutonsChiffres[6] = new JButton("6");
+        boutonsChiffres[7] = new JButton("7");
+        boutonsChiffres[8] = new JButton("8");
+        boutonsChiffres[9] = new JButton("9");*/
+
+        for(int i = 0; i < 10; i++) { // instanciation + actionlistener sur chaque bouton, comme pas de symbole il suffit de prendre la valeur d'index comme titre
+            boutonsChiffres[i] = new JButton(String.valueOf(i));
+            boutonsChiffres[i].addActionListener(this);
+            boutonsChiffres[i].setFont(policeUtilisee);
+            boutonsChiffres[i].setFocusable(false);
+        }
+
+        boutonEffa.setBounds(40, 80, 120,40);
+        boutonSupp.setBounds(180, 80, 120, 40);
+
+        fenetre.add(boutonEffa);
+        fenetre.add(boutonSupp);
 
         fenetre.setResizable(false);
         fenetre.add(zoneAffichage);
