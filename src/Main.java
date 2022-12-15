65f47d6511d05e7a512ddf3a9c32d1ea1885fddc
balex69 <119514045+balex69@users.noreply.github.com>
2022-12-15 18:11:19 +0100

    fonctions finalités

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ public class Main implements ActionListener{
     JFrame fenetre;
     JTextField zoneAffichage;
     JButton[] boutonsChiffres = new JButton[10];
-    JButton[] boutonsFonctions = new JButton[8];
+    JButton[] boutonsFonctions = new JButton[9];
     JButton boutonAddi, boutonSous, boutonMult, boutonDivi;
-    JButton boutonDeci, boutonEgal, boutonSupp, boutonEffa;
+    JButton boutonDeci, boutonEgal, boutonSupp, boutonEffa, boutonRset;
     JPanel panneau;
 
     Font policeUtilisee = new Font("Calibri",Font.BOLD,20);
 
-    double num1 = 0, num2 = 0, result = 0;
+    double num1 = 0, num2 = 0, resultat = 0;
     char operateur;
 
     Main(){
@@ -25,6 +25,7 @@ public class Main implements ActionListener{
         fenetre.setLayout(null);
 
         zoneAffichage = new JTextField();
+        zoneAffichage.setHorizontalAlignment(SwingConstants.RIGHT);
         zoneAffichage.setBounds(40, 20, 260, 40);
         zoneAffichage.setFont(policeUtilisee);
         zoneAffichage.setEditable(false);
@@ -32,11 +33,12 @@ public class Main implements ActionListener{
         boutonAddi = new JButton("+"); // instanciation des boutons de fonction
         boutonSous = new JButton("-");
         boutonMult = new JButton("x");
-        boutonDivi = new JButton("%");
+        boutonDivi = new JButton("/");
         boutonDeci = new JButton(".");
         boutonEgal = new JButton("=");
         boutonSupp = new JButton("<-");
-        boutonEffa = new JButton("C");
+        boutonEffa = new JButton("CE");
+        boutonRset = new JButton("C");
 
         boutonsFonctions[0] = boutonAddi; // ajout à l'array
         boutonsFonctions[1] = boutonSous;
@@ -46,8 +48,9 @@ public class Main implements ActionListener{
         boutonsFonctions[5] = boutonEgal;
         boutonsFonctions[6] = boutonSupp;
         boutonsFonctions[7] = boutonEffa;
+        boutonsFonctions[8] = boutonRset;
 
-        for(int i = 0; i < 8; i++) { // ajout de l'actionlistener à chaque bouton
+        for(int i = 0; i < 9; i++) { // ajout de l'actionlistener à chaque bouton
             boutonsFonctions[i].addActionListener(this);
             boutonsFonctions[i].setFont(policeUtilisee);
             boutonsFonctions[i].setHorizontalAlignment(SwingConstants.CENTER);
@@ -75,10 +78,12 @@ public class Main implements ActionListener{
             boutonsChiffres[i].setFocusable(false);
         }
 
-        boutonEffa.setBounds(40, 80, 120,40);
-        boutonSupp.setBounds(180, 80, 120, 40);
+        boutonEffa.setBounds(40, 80, 70,40);
+        boutonRset.setBounds(135, 80, 70, 40);
+        boutonSupp.setBounds(230, 80, 70, 40);
 
         fenetre.add(boutonEffa);
+        fenetre.add(boutonRset);
         fenetre.add(boutonSupp);
 
         JPanel positionTouches = new JPanel();
@@ -118,5 +123,69 @@ public class Main implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        for(int i = 0 ; i < 10 ; i++) {
+            if(e.getSource() == boutonsChiffres[i]) {
+                zoneAffichage.setText(zoneAffichage.getText().concat(String.valueOf(i)));
+            }
+        }
+        if(e.getSource() == boutonDeci) {
+            zoneAffichage.setText(zoneAffichage.getText().concat("."));
+        }
+        if(e.getSource() == boutonAddi) {
+            num1 = Double.parseDouble(zoneAffichage.getText());
+            operateur = '+';
+            zoneAffichage.setText("");
+        }
+        if(e.getSource() == boutonSous) {
+            num1 = Double.parseDouble(zoneAffichage.getText());
+            operateur = '-';
+            zoneAffichage.setText("");
+        }
+        if(e.getSource() == boutonMult) {
+            num1 = Double.parseDouble(zoneAffichage.getText());
+            operateur = 'x';
+            zoneAffichage.setText("");
+        }
+        if(e.getSource() == boutonDivi) {
+            num1 = Double.parseDouble(zoneAffichage.getText());
+            operateur = '/';
+            zoneAffichage.setText("");
+        }
+        if(e.getSource() == boutonEgal) {
+            num2 = Double.parseDouble(zoneAffichage.getText());
+
+            switch(operateur) {
+                case '+':
+                    resultat = num1 + num2;
+                    break;
+                case '-':
+                    resultat = num1 - num2;
+                    break;
+                case 'x':
+                    resultat = num1 * num2;
+                    break;
+                case '/':
+                    resultat = num1 / num2;
+                    break;
+            }
+            zoneAffichage.setText(String.valueOf(resultat));
+            num1 = resultat;
+        }
+        if(e.getSource() == boutonEffa) {
+            zoneAffichage.setText("");
+        }
+        if(e.getSource() == boutonSupp) {
+            String string = zoneAffichage.getText();
+            zoneAffichage.setText("");
+            for(int i = 0 ; i < string.length()-1 ; i++) {
+                zoneAffichage.setText(zoneAffichage.getText()+string.charAt(i));
+            }
+        }
+        if(e.getSource() == boutonRset) {
+            zoneAffichage.setText("");
+            num1 = 0;
+            num2 = 0;
+            resultat = 0;
+        }
     }
 }
